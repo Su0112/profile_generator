a6c9295039c46ca49af0e832735be2b6538532ce
SuShruthi <shruthacm@gmail.com>
2023-04-18 14:08:12 -0700

    challenge completed

--- survey.js
+++ survey.js
@@ -5,8 +5,19 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  rl.close();
+rl.question("What's your name? Nicknames are also acceptable :) ", (name) => {
+  rl.question("What's an activity you like doing? ", (activity) => {
+    rl.question("What do you listen to while doing that? ", (music) => {
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.)? ", (meal) => {
+        rl.question("What's your favourite thing to eat for that meal? ", (food) => {
+          rl.question("Which sport is your absolute favourite? ", (sport) => {
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (superpower) => {
+              console.log(`My name is ${name}. I love ${activity} and listening to ${music}. My favorite meal of the day is ${meal}, and my favorite food for that is ${food}. I'm a huge fan of ${sport} and my superpower is ${superpower}.`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
 });
